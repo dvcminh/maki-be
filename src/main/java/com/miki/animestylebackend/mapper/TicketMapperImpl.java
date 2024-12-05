7396cb8d47db2060772e5a13785ba00807e43433
Vũ Đức Minh <vuducminh210503@gmail.com>
2024-12-06 00:03:02 +0700

    fix: fix ticket dto

--- src/main/java/com/miki/animestylebackend/mapper/TicketMapperImpl.java
+++ src/main/java/com/miki/animestylebackend/mapper/TicketMapperImpl.java
@@ -22,7 +22,7 @@ public class TicketMapperImpl implements TicketMapper{
             return null;
         }
         TicketData ticketData = new TicketData();
-        ticketData.setUuid(ticket.getId());
+        ticketData.setTicketId(ticket.getId());
         ticketData.setUser(userMapper.toUserData(ticket.getUser()));
         ticketData.setShop(shopMapper.toShopData(ticket.getShop()));
         ticketData.setDriver(driverMapper.toDriverData(ticket.getDriver()));
